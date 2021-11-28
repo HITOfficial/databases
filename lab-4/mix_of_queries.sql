4b6d98f2bfdf22ca36c8ba443cc18efaeffdaba5
Tomasz <tomaszbochnak00@gmail.com>
2021-11-28 01:44:53 +0100

    queries using: subqueries, joins aggregates

--- lab-4/mix_of_queries.sql
+++ lab-4/mix_of_queries.sql
@@ -154,7 +154,36 @@ WHERE c.CustomerID NOT IN (
 )
 
 -- Dla kazdego klienta najczesciej zamawiana kategorie w dwóch wersjach.
-SELECT TOP 13 c.CompanyName, ca.CategoryName, MAX(ca.CategoryID) AS 'MOST POPULAR CATEGORY' FROM Customers c
+USE Northwind
+SELECT c.CompanyName, ca.CategoryName, COUNT(ca.CategoryID)
+FROM CUSTOMERS c
+INNER JOIN Orders o
+ON c.CustomerID = o.CustomerID
+INNER JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+INNER JOIN Products p
+ON od.ProductID = p.ProductID
+INNER JOIN Categories ca
+ON p.ProductID = ca.CategoryID
+WHERE ca.CategoryID = (
+	SELECT TOP 1 Categories.CategoryID
+	FROM Categories
+	INNER JOIN Products
+	ON Categories.CategoryID =  Products.CategoryID
+	INNER JOIN [Order Details]
+	ON Products.ProductID=  [Order Details].ProductID
+	INNER JOIN Orders
+	ON [Order Details].OrderID = Orders.OrderID
+	INNER JOIN Customers
+	ON Orders.CustomerID = Customers.CustomerID
+	WHERE Customers.CustomerID = c.CustomerID
+	GROUP BY Customers.CustomerID, Categories.CategoryID
+	ORDER BY COUNT(Categories.CategoryID) DESC
+)
+GROUP BY c.CompanyName, ca.CategoryName
+
+SELECT c.CompanyName, ca.CategoryName, COUNT(ca.CategoryID)
+FROM CUSTOMERS c
 INNER JOIN Orders o
 ON c.CustomerID = o.CustomerID
 INNER JOIN [Order Details] od
@@ -164,7 +193,19 @@ ON od.ProductID = p.ProductID
 INNER JOIN Categories ca
 ON p.ProductID = ca.CategoryID
 GROUP BY c.CustomerID, c.CompanyName, ca.CategoryName
-ORDER BY 3 DESC
+HAVING ca.CategoryName = (
+	SELECT TOP 1 Categories.CategoryName
+	FROM Categories
+	INNER JOIN Products
+	ON Categories.CategoryID =  Products.CategoryID
+	INNER JOIN [Order Details]
+	ON Products.ProductID=  [Order Details].ProductID
+	INNER JOIN Orders
+	ON [Order Details].OrderID = Orders.OrderID
+	WHERE Orders.CustomerID = c.CustomerID
+	GROUP BY Orders.CustomerID, Categories.CategoryName
+	ORDER BY COUNT(Categories.CategoryName) DESC
+)
 
  --Podział na company, year month i suma freight
  SELECT c.CompanyName, YEAR(o.OrderDate), MONTH(o.OrderDate), o.Freight + SUM(od.Quantity*od.UnitPrice*(1-od.Discount))
@@ -174,3 +215,139 @@ ORDER BY 3 DESC
  INNER JOIN [Order Details] od
  ON o.OrderID = od.OrderID
  GROUP BY c.CustomerID, c.CompanyName, YEAR(o.OrderDate), MONTH(o.OrderDate), o.Freight
+ 
+ --Wypisać wszystkich czytelników, którzy nigdy nie wypożyczyli książki dane adresowe i podział czy ta osoba jest dzieckiem (joiny, in, exists)
+ USE library
+ SELECT m.firstname, m.lastname, 'adult' AS 'adult / juvenile', a.street + ' ' + a.city + ' ' + a.zip AS 'adress'
+ FROM member m
+ INNER JOIN adult a
+ ON m.member_no = a.member_no
+ WHERE m.member_no NOT IN (
+	SELECT mm.member_no
+	FROM member mm
+	INNER JOIN loanhist lh
+	ON mm.member_no = lh.member_no
+ )
+ UNION
+ SELECT m.firstname, m.lastname, 'juvenile' AS 'adult / juvenile', a.street + ' ' + a.city + ' ' + a.zip AS 'adress'
+ FROM member m
+ INNER JOIN juvenile j
+ ON m.member_no = j.member_no
+ INNER JOIN adult a
+ ON j.adult_member_no = a.member_no
+ WHERE m.member_no NOT IN (
+	SELECT mm.member_no
+	FROM member mm
+	INNER JOIN loanhist lh
+	ON mm.member_no = lh.member_no
+ )
+
+ --Najczęściej wybierana kategoria w 1997 dla każdego klienta
+ USE Northwind
+ SELECT c.CompanyName, ca.CategoryName FROM Customers c
+ INNER JOIN Orders o
+ ON c.CustomerID = o.CustomerID
+ INNER JOIN [Order Details] od
+ ON o.OrderID = od.OrderID
+ INNER JOIN Products p 
+ ON od.ProductID = p.ProductID
+ INNER JOIN Categories ca 
+ ON p.CategoryID = ca.CategoryID
+WHERE ca.CategoryName = (
+	SELECT TOP 1 ccaa.CategoryName FROM Customers cc
+	INNER JOIN Orders oo
+	ON cc.CustomerID = oo.CustomerID
+	INNER JOIN [Order Details] oodd
+	ON oo.OrderID = oodd.OrderID
+	INNER JOIN Products pp
+	ON oodd.ProductID = pp.ProductID
+	INNER JOIN Categories ccaa 
+	ON pp.CategoryID = ccaa.CategoryID
+	WHERE c.CustomerID = cc.CustomerID AND YEAR(o.OrderDate) = 1997
+	GROUP BY cc.CustomerID, cc.CompanyName, ccaa.CategoryName
+	ORDER BY COUNT(ccaa.CategoryName) DESC
+)
+
+ --Dla każdego czytelnika imię nazwisko, suma książek wypożyczony przez tą osobę i jej dzieci, który żyje w Arizona ma mieć więcej niż 2 dzieci lub kto żyje w Kalifornii ma mieć więcej niż 3 dzieci
+ USE library
+ SELECT m.firstname, m.lastname, m.member_no,
+ (SELECT COUNT(loanhist.isbn) FROM loanhist WHERE loanhist.member_no = m.member_no) +
+ (SELECT COUNT(loanhist.isbn) FROM loanhist WHERE loanhist.member_no IN 
+	(SELECT juvenile.member_no FROM juvenile WHERE juvenile.adult_member_no = m.member_no)
+) AS 'loaned books'
+ FROM member m
+ INNER JOIN adult a
+ ON m.member_no = a.member_no
+ INNER JOIN juvenile j
+ ON a.member_no = j.adult_member_no
+ WHERE a.state = 'AZ'
+ GROUP BY m.member_no, m.firstname, m.lastname
+ HAVING COUNT(j.member_no) > 2
+ UNION
+ SELECT m.firstname, m.lastname, m.member_no,
+ (SELECT COUNT(loanhist.isbn) FROM loanhist WHERE loanhist.member_no = m.member_no) +
+ (SELECT COUNT(loanhist.isbn) FROM loanhist WHERE loanhist.member_no IN 
+	(SELECT juvenile.member_no FROM juvenile WHERE juvenile.adult_member_no = m.member_no)
+)
+ FROM member m
+ INNER JOIN adult a
+ ON m.member_no = a.member_no
+ INNER JOIN juvenile j
+ ON a.member_no = j.adult_member_no
+ WHERE a.state = 'CA'
+ GROUP BY m.member_no, m.firstname, m.lastname
+ HAVING COUNT(j.member_no) > 3
+
+-- Jaki był najpopularniejszy autor wśród dzieci w Arizonie w 2001
+SELECT TOP 1 t.author FROM member m
+INNER JOIN juvenile j
+ON m.member_no = j.member_no
+INNER JOIN adult a 
+ON j.adult_member_no = a.member_no
+INNER JOIN loanhist lh
+ON lh.member_no = m.member_no
+RIGHT JOIN title t
+ON lh.title_no = t.title_no
+WHERE a.state = 'AZ' AND YEAR(lh.in_date) = 2001 
+GROUP BY t.author
+ORDER BY COUNT(t.title_no) DESC
+
+-- Dla każdego dziecka wybierz jego imię nazwisko, adres, imię i nazwisko rodzica i ilość książek, które oboje przeczytali w 2001
+SELECT m.firstname + ' ' + m.lastname AS 'Children',
+(SELECT member.firstname + ' ' +  member.lastname FROM member  WHERE j.adult_member_no = member.member_no) AS 'Parent',
+(SELECT adult.street + ' ' +  adult.city FROM adult  WHERE j.adult_member_no = adult.member_no) AS 'Adress',
+(SELECT COUNT(loanhist.title_no) FROM loanhist WHERE loanhist.member_no = m.member_no AND YEAR(loanhist.due_date) = 2001) +
+(SELECT COUNT(loanhist.title_no) FROM loanhist WHERE loanhist.member_no = j.adult_member_no AND YEAR(loanhist.due_date) = 2001)
+FROM member m
+INNER JOIN juvenile j
+ON m.member_no = j.member_no
+
+-- Kategorie które w roku 1997 grudzień były obsłużone wyłącznie przez ‘United Package’
+USE Northwind
+SELECT c.CategoryName 
+FROM Orders o
+INNER JOIN Suppliers s
+ON s.SupplierID = o.ShipVia
+INNER JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+INNER JOIN Products p
+ON od.ProductID = p.ProductID
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID
+WHERE YEAR(o.OrderDate) = 1997 AND MONTH(o.OrderDate) = 12
+GROUP BY c.CategoryID, c.CategoryName
+HAVING (
+	SELECT COUNT(Orders.OrderID)
+	FROM Orders
+	INNER JOIN [Order Details]
+	ON Orders.OrderID = [Order Details].OrderID
+	INNER JOIN Products 
+	ON [Order Details].ProductID = Products.ProductID
+	INNER JOIN Categories
+	ON Products.CategoryID = Categories.CategoryID 
+	WHERE Orders.ShipVia NOT IN (
+		SELECT Shippers.ShipperID
+		FROM Shippers
+		WHERE Shippers.CompanyName <> 'United Package'
+	) AND YEAR(Orders.OrderDate) = 1997 AND MONTH(Orders.OrderDate) = 12 AND Categories.CategoryID = c.CategoryID
+) = 0
