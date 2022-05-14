b03a32890ca9350f44548cd59885b81f439ac275
hit <tomaszbochnak00@gmail.com>
2022-05-14 17:37:31 +0200

    simple adding new items to Database using Entity Framework

---
+++ Entity-Framework-Product/bin/Debug/net6.0/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace TomaszBochnakEFProducts
+{
+    internal class ProductContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }    
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("DataSource=ProductsDatabase");
+        }
+    }
+}
---
+++ Entity-Framework-Product/bin/Debug/net6.0/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TomaszBochnakEFProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insert Product Name:");
+
+            string prodName = Console.ReadLine();
+            Console.WriteLine("Products list in DB:");
+            ProductContext productContext = new ProductContext();
+            Product product = new Product { ProductName = prodName};
+            productContext.Products.Add(product);
+            productContext.SaveChanges();
+
+            var query = from prod in productContext.Products
+                        select prod.ProductName;
+
+
+            foreach (var pName in query)
+            {
+                Console.WriteLine(pName);
+            }
+        }
+
+    }
+}
+
